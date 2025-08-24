8b34eea9df364206dcb0c28e7e7aca38a90e41b3
ghazibayan905-cmd <ghazibayan905@gmail.com>
2025-08-24 23:41:06 +0300

    use silverapp

--- lib/ui/views/pages/home/home_screen.dart
+++ lib/ui/views/pages/home/home_screen.dart
@@ -1,13 +1,7 @@
-import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:http/http.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:simple_post_app_challenge/core/constant/app_colors.dart';
 import 'package:simple_post_app_challenge/core/constant/app_images.dart';
 import 'package:simple_post_app_challenge/core/constant/app_routes.dart';
 import 'package:simple_post_app_challenge/core/enum/request_type.dart';
@@ -16,8 +10,6 @@ import 'package:simple_post_app_challenge/core/network/network_utils.dart';
 import 'package:simple_post_app_challenge/core/post_model.dart';
 import 'package:simple_post_app_challenge/ui/views/pages/home/widgets/bottom_sheet.dart';
 import 'package:simple_post_app_challenge/ui/views/pages/home/widgets/container_post.dart';
-import 'package:simple_post_app_challenge/ui/views/pages/post_Details/post_Screen.dart';
-import 'package:simple_post_app_challenge/ui/views/pages/add_post.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({Key? key}) : super(key: key);
@@ -30,6 +22,7 @@ class _HomescreenState extends State<Homescreen> {
   List<PostModel> posts = [];
   List<CommentModel> comment = [];
   List<bool> expandContainers = [];
+
   @override
   void initState() {
     super.initState();
@@ -55,9 +48,11 @@ class _HomescreenState extends State<Homescreen> {
           expandContainers = List.filled(posts.length, false);
           for (int i = 0; i < posts.length; i++) {
             Future.delayed(const Duration(milliseconds: 500), () {
-              setState(() {
-                expandContainers[i] = true;
-              });
+              if (mounted) {
+                setState(() {
+                  expandContainers[i] = true;
+                });
+              }
             });
           }
         });
@@ -93,10 +88,8 @@ class _HomescreenState extends State<Homescreen> {
       }
 
       return [];
-    } on SocketException {
-      throw Exception("لا يوجد اتصال بالإنترنت");
     } catch (e) {
-      throw Exception("خطأ غير متوقع: $e");
+      throw Exception(" arror an occured: $e");
     }
   }
 
@@ -104,54 +97,117 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 9.w),
-            child: InkWell(
-              onTap: () async {
-                final result = await Navigator.pushNamed(
-                  context,
-                  AppRoutes.addPost,
-                );
-                if (result == true) _loadPosts();
-              },
-              child: SvgPicture.asset(Appimage.create_note),
-            ),
-          ),
-        ],
-        backgroundColor: const Color.fromARGB(255, 188, 188, 189),
-      ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(child: Image.asset(Appimage.socialhub)),
-              SizedBox(height: 20.h),
-
-              for (var i = 0; i < posts.length; i++) ...[
-                CustomContainer(
-                  width: expandContainers[i],
-
-                  title: posts[i].title.toString(),
-                  post: posts[i],
-                  index: i,
-                  body: posts[i].body ?? "no body",
-                  onCommentPressed: () async {
-                    final comments = await getComments(posts[i].id!);
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return BuildSheet(comments: comments);
-                      },
-                    );
-                  },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              floating: false,
+              snap: false,
+              expandedHeight: 180.h,
+              backgroundColor: Colors.transparent,
+              flexibleSpace: ClipRRect(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(40.r),
                 ),
-                SizedBox(height: 16.h),
-              ],
-            ],
-          ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.white.withOpacity(0.3),
+                        const Color.fromARGB(
+                          255,
+                          194,
+                          192,
+                          192,
+                        ).withOpacity(0.2),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: SafeArea(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Center(
+                              child: AnimatedScale(
+                                scale: 1.1,
+                                duration: const Duration(milliseconds: 600),
+                                curve: Curves.easeInOut,
+                                child: Image.asset(
+                                  Appimage.socialhub,
+                                  height: 100.h,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () async {
+                              final result = await Navigator.pushNamed(
+                                context,
+                                AppRoutes.addPost,
+                              );
+                              if (result == true) _loadPosts();
+                            },
+                            borderRadius: BorderRadius.circular(50.r),
+                            child: Container(
+                              padding: EdgeInsets.all(8.w),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.3),
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    blurRadius: 6,
+                                    offset: const Offset(2, 2),
+                                  ),
+                                ],
+                              ),
+                              child: SvgPicture.asset(
+                                Appimage.create_note,
+                                height: 28.h,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 20.h)),
+
+            SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(bottom: 16.h),
+                  child: CustomContainer(
+                    width: expandContainers[index],
+                    title: posts[index].title.toString(),
+                    post: posts[index],
+                    index: index,
+                    body: posts[index].body ?? "no body",
+                    onCommentPressed: () async {
+                      final comments = await getComments(posts[index].id!);
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return BuildSheet(comments: comments);
+                        },
+                      );
+                    },
+                  ),
+                );
+              }, childCount: posts.length),
+            ),
+          ],
         ),
       ),
     );
